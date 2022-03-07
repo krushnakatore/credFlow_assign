726b5c6a7888e0a9a4e4e022dd10e99b5be83767
Krushna Katore <91548932+krushnakatore@users.noreply.github.com>
2022-03-07 19:14:41 +0530

    question

---
+++ spiralTraversing.js
@@ -0,0 +1,41 @@
+let arr = [
+  [1, 2, 3],
+  [5, 6, 7],
+  [10, 9, 8],
+  [7, 8, 9],
+];
+
+let N = arr.length;
+let M = arr[0].length;
+
+var traverse = "";
+let top = 0;
+let bottom = N - 1;
+let right = M - 1;
+let left = 0;
+let count = 0;
+
+while (count < N * M) {
+  for (var r = top; r <= bottom && count < N * M; r++) {
+    traverse = traverse + arr[r][left] + " ";
+    count++;
+  }
+  left++;
+
+  for (var r = left; r <= right && count < N * M; r++) {
+    traverse = traverse + arr[bottom][r] + " ";
+    count++;
+  }
+  bottom--;
+  for (var r = bottom; r >= top && count < N * M; r--) {
+    traverse = traverse + arr[r][right] + " ";
+    count++;
+  }
+  right--;
+  for (var r = right; r >= left && count < N * M; r--) {
+    traverse = traverse + arr[top][r] + " ";
+    count++;
+  }
+  top++;
+}
+console.log(traverse);
